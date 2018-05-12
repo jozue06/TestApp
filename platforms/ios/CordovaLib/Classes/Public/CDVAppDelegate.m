60978a24b97fe6f6868045266a9f47c7ad901199
Jozue06 <jozue06@gmail.com>
2018-05-12 15:42:44 -0700

    still working on push and pusher

--- platforms/ios/CordovaLib/Classes/Public/CDVAppDelegate.m
+++ platforms/ios/CordovaLib/Classes/Public/CDVAppDelegate.m
@@ -75,6 +75,22 @@
 // only valid if 40x-Info.plist specifies a protocol to handle
 - (BOOL)application:(UIApplication*)application openURL:(NSURL*)url sourceApplication:(NSString*)sourceApplication annotation:(id)annotation
 {
+    @import UIKit
+    @import PushNotifications
+    
+    @UIApplicationMain
+    class AppDelegate: UIResponder, UIApplicationDelegate {
+        
+        var window: UIWindow?
+        let pushNotifications = PushNotifications.shared
+        
+        func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+            self.pushNotifications.start(instanceId: "YOUR_INSTANCE_ID")
+            self.pushNotifications.registerForRemoteNotifications()
+            return true
+        }
+    }
+    
     if (!url) {
         return NO;
     }
@@ -97,6 +113,8 @@
 
     return YES;
 }
+
+
 
 #if __IPHONE_OS_VERSION_MAX_ALLOWED < 90000  
 - (NSUInteger)application:(UIApplication*)application supportedInterfaceOrientationsForWindow:(UIWindow*)window

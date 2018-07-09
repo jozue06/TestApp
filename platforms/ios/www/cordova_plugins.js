0fb6afb36015314c3a7358a760ae9660ad5ec10f
Jozue06 <jozue06@gmail.com>
2018-07-08 21:06:36 -0700

    snot

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,9 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-cocoapod-support": "1.5.0"
+};
+// BOTTOM OF METADATA
+});
